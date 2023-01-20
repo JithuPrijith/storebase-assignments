46dbf94233e43d4626d6da4e30a620e993c79ee9
JithuPrijith <prijith@storebase.in>
2023-01-20 13:39:12 +0530

    call back hello world

--- callbacks.js
+++ callbacks.js
@@ -1,3 +1,19 @@
-for (let i = 0; i < 10; i++) {
-  setTimeout(() => console.log(i));
+var word = "HELLO WORLD";
+
+function arraysplit(data) {
+  let array = data.split("");
+  for (let letter of array) {
+    return letter;
+  }
+}
+
+function setDelay(data, i) {
+  setTimeout(async () => {
+    let res =await arraysplit(data);
+    console.log(res);
+  }, i * 1000);
+}
+
+for (let i = 1; i < 10; i++) {
+  setDelay(word, i);
 }
