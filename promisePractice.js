a6c1219c0b300c1ae2a0e30d38b118029abe6768
JithuPrijith <prijith@storebase.in>
2023-01-21 10:45:20 +0530

    promise practice completed

---
+++ promisePractice.js
@@ -0,0 +1,26 @@
+// using callback
+function wait3000(callback) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      callback();
+    }, 3000);
+  });
+}
+
+// wait3000(() => {
+//   console.log("waited and worked");
+// });
+
+//  using then
+
+function wait4000() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 4000);
+  });
+}
+
+wait4000().then(() => {
+  console.log("waited 4000");
+});
