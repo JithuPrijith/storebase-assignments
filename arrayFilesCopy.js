07344851ec81a38f37c2ff92f934d70ccf716158
JithuPrijith <prijith@storebase.in>
2023-01-21 16:39:15 +0530

    file copy parallel using promiseAll

---
+++ arrayFilesCopy.js
@@ -0,0 +1,26 @@
+var fs = require("fs");
+const path = require("path");
+
+function copyFile(src, dest) {
+  let srcPath = path.join(__dirname + "/" + src);
+  let destPath = path.join(__dirname + "/" + dest);
+  return new Promise((resolve, reject) => {
+    try {
+      fs.cp(srcPath, destPath, (err) => {
+        err ? reject(err) : resolve("success");
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+Promise.all([
+  copyFile("sourceDir/hai.js", "destDir/hai.js"),
+  copyFile("sourceDir/hello.js", "destDir/hello.js"),
+])
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err, "err"));
+
+// let sourceArray = ["sourceDir/hai.js", "sourceDir/hello.js"];
+// let destArray = ["destDir/hai.js", "destDir/hello.js"];
